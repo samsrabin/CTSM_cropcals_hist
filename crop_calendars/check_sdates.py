9ef0c1c6e23131c35e089abd725554bee8069def
Sam Rabin <sam.rabin@gmail.com>
2021-11-23 11:40:49 -0700

    Mostly variable renames. Some updates for new utils behaviors.

--- crop_calendars/check_sdates.py
+++ crop_calendars/check_sdates.py
@@ -30,45 +30,34 @@ warnings.filterwarnings("ignore", message="__len__ for multi-part geometries is 
 warnings.filterwarnings("ignore", message="Iteration over multi-part geometries is deprecated and will be removed in Shapely 2.0. Use the `geoms` property to access the constituent parts of a multi-part geometry.")
 
 
-# %% Import realized sowing and harvest dates
-
-# Either the name of a file within $indir, or a pattern that will return a list of files.
-pattern = "*h2.*-01-01-00000.nc"
-
-# Get list of all files in $indir matching $pattern
-filelist = glob.glob(indir + pattern)
-
-# Import
-this_ds = utils.import_ds(filelist, myVars=["SDATES", "HDATES"], myVegtypes=utils.define_mgdcrop_list())
-sdates_gridded = utils.grid_one_variable(\
-    this_ds, 
-    "SDATES", 
-    time=1)
+# %% Import sowing and harvest dates
+dates_ds = utils.import_ds(glob.glob(indir + "*h2.*-01-01-00000.nc"), \
+    myVars=["SDATES", "HDATES"], 
+    myVegtypes=utils.define_mgdcrop_list())
 
 
 # %% Check that sowing dates were the same in 2000 and 2001
 
-this_ds.SDATES.isel(time=2).values[this_ds.SDATES.isel(time=1).values
-                            != this_ds.SDATES.isel(time=2).values]
+dates_ds.SDATES.isel(time=2).values[dates_ds.SDATES.isel(time=1).values
+                            != dates_ds.SDATES.isel(time=2).values]
 
-this_ds.HDATES.isel(time=2).values[this_ds.SDATES.isel(time=1).values
-                            != this_ds.SDATES.isel(time=2).values]
+dates_ds.HDATES.isel(time=2).values[dates_ds.SDATES.isel(time=1).values
+                            != dates_ds.SDATES.isel(time=2).values]
 
 
 # %% Import expected sowing dates
 
-
-sdate_inFile = "/Volumes/Reacher/CESM_work/crop_dates/sdates_ggcmi_crop_calendar_phase3_v1.01.2000-2000.nc"
+sdate_inFile = "/Volumes/Reacher/CESM_work/crop_dates/sdates_ggcmi_crop_calendar_phase3_v1.01_nninterp.2000-2000.nc"
 
 # Get run info:
 # Max number of growing seasons per year
-if "mxgrowseas" in this_ds:
-    mxgrowseas = this_ds.dims["mxgrowseas"]
+if "mxgrowseas" in dates_ds:
+    mxgrowseas = dates_ds.dims["mxgrowseas"]
 else:
     mxgrowseas = 1
     
 # Which vegetation types were simulated?
-itype_veg_toImport = np.unique(this_ds.patches1d_itype_veg)
+itype_veg_toImport = np.unique(dates_ds.patches1d_itype_veg)
 
 sdate_varList = []
 for i in itype_veg_toImport:
@@ -93,6 +82,11 @@ sdates_rx = utils.import_ds(sdate_inFile, myVars=sdate_varList)
 # import importlib
 # importlib.reload(utils)
 
+sdates_gridded = utils.grid_one_variable(\
+    dates_ds, 
+    "SDATES", 
+    time__values="2001")
+
 outdir = f"{indir}/sdates"
 if not os.path.exists(outdir):
     os.makedirs(outdir)
@@ -111,13 +105,13 @@ def make_map(ax, this_map, this_title):
 
 ny = 2
 nx = 1
-for i, vt_str in enumerate(this_ds.vegtype_str.values):
+for i, vt_str in enumerate(dates_ds.vegtype_str.values):
     
     print(vt_str)
     fig = plt.figure(figsize=(8,10))
         
     # Input
-    vt = this_ds.ivt.values[i]
+    vt = dates_ds.ivt.values[i]
     thisVar = f"sdate1_{vt}"
     if thisVar in sdates_rx:
         ax = fig.add_subplot(ny,nx,1,projection=ccrs.PlateCarree())
@@ -141,31 +135,32 @@ for i, vt_str in enumerate(this_ds.vegtype_str.values):
 print("Done!")
 
 
-
 # %% Map harvest and sowing dates
 
 # import importlib
 # importlib.reload(utils)
 
 hdates00_gridded = utils.grid_one_variable(\
-    this_ds, 
+    dates_ds, 
     "HDATES", 
-    time=1)
+    time__indices=1,
+    mxharvests__indices=0)
 
 sdates00_gridded = utils.grid_one_variable(\
-    this_ds, 
+    dates_ds, 
     "SDATES", 
-    time=1)
+    time__indices=1)
 
 hdates01_gridded = utils.grid_one_variable(\
-    this_ds, 
+    dates_ds, 
     "HDATES", 
-    time=2)
+    time__indices=2,
+    mxharvests__indices=0)
 
 sdates01_gridded = utils.grid_one_variable(\
-    this_ds, 
+    dates_ds, 
     "SDATES", 
-    time=2)
+    time__indices=2)
 
 outdir = f"{indir}/sowing_and_harvest"
 if not os.path.exists(outdir):
@@ -185,7 +180,7 @@ def make_map(ax, this_map, this_title):
 
 ny = 2
 nx = 2
-for i, vt_str in enumerate(this_ds.vegtype_str.values):
+for i, vt_str in enumerate(dates_ds.vegtype_str.values):
     
     print(vt_str)
     fig = plt.figure(figsize=(12,8))
