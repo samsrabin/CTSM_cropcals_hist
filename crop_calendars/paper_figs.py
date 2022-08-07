eef13f81fe70bca2a7b88914b7052d00c6cd3db0
Sam Rabin <sam.rabin@gmail.com>
2022-08-07 16:22:37 -0600

    Refactored some of import_output() into convert_axes_timemxharvests_gs().

--- crop_calendars/paper_figs.py
+++ crop_calendars/paper_figs.py
@@ -34,105 +34,8 @@ warnings.filterwarnings("ignore", message="All-NaN slice encountered")
 
 # %% Define functions
 
-def import_output(filename, myVars, y1=None, yN=None, constantVars=None, myVegtypes=utils.define_mgdcrop_list(), 
-                  sdates_rx_ds=None, gdds_rx_ds=None, verbose=False):
-   
-   # Minimum harvest threshold allowed in PlantCrop()
-   gdd_min = 50
-   
-   # Import
-   this_ds = utils.import_ds(filename,
-                             myVars=myVars,
-                             myVegtypes=myVegtypes)
-   
-   # Trim to years of interest (do not include extra year needed for finishing last growing season)
-   if y1 and yN:
-      this_ds = check_and_trim_years(y1, yN, this_ds)
-   else: # Assume including all growing seasons except last complete one are "of interest"
-      y1 = this_ds.time.values[0].year
-      yN = this_ds.time.values[-1].year - 2
-      this_ds = check_and_trim_years(y1, yN, this_ds)
-      
-   # What vegetation types are included?
-   vegtype_list = [x for x in this_ds.vegtype_str.values if x in this_ds.patches1d_itype_veg_str.values]
-   
-   # Check for consistency among sowing/harvest date/year info
-   date_vars = ["SDATES_PERHARV", "SYEARS_PERHARV", "HDATES", "HYEARS"]
-   all_nan = np.full(this_ds[date_vars[0]].shape, True)
-   all_nonpos = np.full(this_ds[date_vars[0]].shape, True)
-   all_pos = np.full(this_ds[date_vars[0]].shape, True)
-   for v in date_vars:
-      all_nan = all_nan & np.isnan(this_ds[v].values)
-      all_nonpos = all_nonpos & (this_ds[v].values <= 0)
-      all_pos = all_pos & (this_ds[v].values > 0)
-   if np.any(np.bitwise_not(all_nan | all_nonpos | all_pos)):
-      raise RuntimeError("Inconsistent missing/present values on mxharvests axis")
-   
-   # When doing transient runs, it's somehow possible for crops in newly-active patches to be *already alive*. They even have a sowing date (idop)! This will of course not show up in SDATES, but it does show up in SDATES_PERHARV. 
-   # I could put the SDATES_PERHARV dates into where they "should" be, but instead I'm just going to invalidate those "seasons."
-   #
-   # In all but the last calendar year, which patches had no sowing?
-   no_sowing_yp = np.all(np.isnan(this_ds.SDATES.values[:-1,:,:]), axis=1)
-   # In all but the first calendar year, which harvests' jdays are < their sowings' jdays? (Indicates sowing the previous calendar year.)
-   hsdate1_gt_hdate1_yp = this_ds.SDATES_PERHARV.values[1:,0,:] > this_ds.HDATES.values[1:,0,:]
-   # Where both, we have the problem.
-   falsely_alive_yp = no_sowing_yp & hsdate1_gt_hdate1_yp
-   if np.any(falsely_alive_yp):
-      print(f"Warning: {np.sum(falsely_alive_yp)} patch-seasons being ignored: Seemingly sown the year before harvest, but no sowings occurred that year.")
-      falsely_alive_yp = np.concatenate((np.full((1, this_ds.dims["patch"]), False),
-                                       falsely_alive_yp),
-                                       axis=0)
-      falsely_alive_y1p = np.expand_dims(falsely_alive_yp, axis=1)
-      dummy_false_y1p = np.expand_dims(np.full_like(falsely_alive_yp, False), axis=1)
-      falsely_alive_yhp = np.concatenate((falsely_alive_y1p, dummy_false_y1p), axis=1)
-      for v in this_ds.data_vars:
-         if this_ds[v].dims != ("time", "mxharvests", "patch"):
-            continue
-         this_ds[v] = this_ds[v].where(~falsely_alive_yhp)
-   
-   # Get growing season length
-   this_ds["GSLEN_PERHARV"] = get_gs_len_da(this_ds["HDATES"] - this_ds["SDATES_PERHARV"])
-   
-   # Check that some things are constant across years
-   if constantVars:
-      verbose = True
-      t1 = 0 # 0-indexed
-      for v in constantVars:
-         ok = True
-
-         t1_yr = this_ds.gs.values[t1]
-         t1_vals = np.squeeze(this_ds[v].isel(gs=t1).values)
-
-         for t in np.arange(t1+1, this_ds.dims["gs"]):
-            t_yr = this_ds.gs.values[t]
-            t_vals = np.squeeze(this_ds[v].isel(gs=t).values)
-            ok_p = np.squeeze(t1_vals == t_vals)
-            if not np.all(ok_p):
-                  if ok:
-                     print(f"❌ CLM output {v} unexpectedly vary over time:")
-                  ok = False
-                  if verbose:
-                     for thisPatch in np.where(np.bitwise_not(ok_p))[0]:
-                        thisLon = this_ds.patches1d_lon.values[thisPatch]
-                        thisLat = this_ds.patches1d_lat.values[thisPatch]
-                        thisCrop = this_ds.patches1d_itype_veg_str.values[thisPatch]
-                        thisCrop_int = this_ds.patches1d_itype_veg.values[thisPatch]
-                        thisStr = f"   Patch {thisPatch} (lon {thisLon} lat {thisLat}) {thisCrop} ({thisCrop_int})"
-                        if v == "SDATES":
-                              print(f"{thisStr}: Sowing {t1_yr} jday {int(t1_vals[thisPatch])}, {t_yr} jday {int(t_vals[thisPatch])}")
-                        else:
-                              print(f"{thisStr}: {t1_yr} {v} {int(t1_vals[thisPatch])}, {t_yr} {v} {int(t_vals[thisPatch])}")
-                  else:
-                     print(f"{v} timestep {t} does not match timestep {t1}")
-
-         if ok:
-            print(f"✅ CLM output {v} do not vary through {this_ds.dims['gs'] - t1} growing seasons of output.")
-   
-   
-   ##########################################################
-   ### Convert time*mxharvests axes to growingseason axis ###
-   ##########################################################
-   
+# Convert time*mxharvests axes to growingseason axis
+def convert_axes_timemxharvests_gs(this_ds, verbose):
    # How many non-NaN patch-seasons do we expect to have once we're done organizing things?
    Npatch = this_ds.dims["patch"]
    # Because some patches will be planted in the last year but not complete, we have to ignore any finalyear-planted seasons that do complete.
@@ -228,11 +131,107 @@ def import_output(filename, myVars, y1=None, yN=None, constantVars=None, myVegty
    else:
       raise RuntimeError(f"Can't convert time*mxharvests axes to growingseason axis: discrepancy of {discrepancy} patch-seasons")
    
-   
-   #######################
-   ### Complete checks ###
-   #######################
+   return this_ds_gs
 
+
+def import_output(filename, myVars, y1=None, yN=None, constantVars=None, myVegtypes=utils.define_mgdcrop_list(), 
+                  sdates_rx_ds=None, gdds_rx_ds=None, verbose=False):
+   
+   # Minimum harvest threshold allowed in PlantCrop()
+   gdd_min = 50
+   
+   # Import
+   this_ds = utils.import_ds(filename,
+                             myVars=myVars,
+                             myVegtypes=myVegtypes)
+   
+   # Trim to years of interest (do not include extra year needed for finishing last growing season)
+   if y1 and yN:
+      this_ds = check_and_trim_years(y1, yN, this_ds)
+   else: # Assume including all growing seasons except last complete one are "of interest"
+      y1 = this_ds.time.values[0].year
+      yN = this_ds.time.values[-1].year - 2
+      this_ds = check_and_trim_years(y1, yN, this_ds)
+      
+   # What vegetation types are included?
+   vegtype_list = [x for x in this_ds.vegtype_str.values if x in this_ds.patches1d_itype_veg_str.values]
+   
+   # Check for consistency among sowing/harvest date/year info
+   date_vars = ["SDATES_PERHARV", "SYEARS_PERHARV", "HDATES", "HYEARS"]
+   all_nan = np.full(this_ds[date_vars[0]].shape, True)
+   all_nonpos = np.full(this_ds[date_vars[0]].shape, True)
+   all_pos = np.full(this_ds[date_vars[0]].shape, True)
+   for v in date_vars:
+      all_nan = all_nan & np.isnan(this_ds[v].values)
+      all_nonpos = all_nonpos & (this_ds[v].values <= 0)
+      all_pos = all_pos & (this_ds[v].values > 0)
+   if np.any(np.bitwise_not(all_nan | all_nonpos | all_pos)):
+      raise RuntimeError("Inconsistent missing/present values on mxharvests axis")
+   
+   # When doing transient runs, it's somehow possible for crops in newly-active patches to be *already alive*. They even have a sowing date (idop)! This will of course not show up in SDATES, but it does show up in SDATES_PERHARV. 
+   # I could put the SDATES_PERHARV dates into where they "should" be, but instead I'm just going to invalidate those "seasons."
+   #
+   # In all but the last calendar year, which patches had no sowing?
+   no_sowing_yp = np.all(np.isnan(this_ds.SDATES.values[:-1,:,:]), axis=1)
+   # In all but the first calendar year, which harvests' jdays are < their sowings' jdays? (Indicates sowing the previous calendar year.)
+   hsdate1_gt_hdate1_yp = this_ds.SDATES_PERHARV.values[1:,0,:] > this_ds.HDATES.values[1:,0,:]
+   # Where both, we have the problem.
+   falsely_alive_yp = no_sowing_yp & hsdate1_gt_hdate1_yp
+   if np.any(falsely_alive_yp):
+      print(f"Warning: {np.sum(falsely_alive_yp)} patch-seasons being ignored: Seemingly sown the year before harvest, but no sowings occurred that year.")
+      falsely_alive_yp = np.concatenate((np.full((1, this_ds.dims["patch"]), False),
+                                       falsely_alive_yp),
+                                       axis=0)
+      falsely_alive_y1p = np.expand_dims(falsely_alive_yp, axis=1)
+      dummy_false_y1p = np.expand_dims(np.full_like(falsely_alive_yp, False), axis=1)
+      falsely_alive_yhp = np.concatenate((falsely_alive_y1p, dummy_false_y1p), axis=1)
+      for v in this_ds.data_vars:
+         if this_ds[v].dims != ("time", "mxharvests", "patch"):
+            continue
+         this_ds[v] = this_ds[v].where(~falsely_alive_yhp)
+   
+   # Get growing season length
+   this_ds["GSLEN_PERHARV"] = get_gs_len_da(this_ds["HDATES"] - this_ds["SDATES_PERHARV"])
+   
+   # Check that some things are constant across years
+   if constantVars:
+      verbose = True
+      t1 = 0 # 0-indexed
+      for v in constantVars:
+         ok = True
+
+         t1_yr = this_ds.gs.values[t1]
+         t1_vals = np.squeeze(this_ds[v].isel(gs=t1).values)
+
+         for t in np.arange(t1+1, this_ds.dims["gs"]):
+            t_yr = this_ds.gs.values[t]
+            t_vals = np.squeeze(this_ds[v].isel(gs=t).values)
+            ok_p = np.squeeze(t1_vals == t_vals)
+            if not np.all(ok_p):
+                  if ok:
+                     print(f"❌ CLM output {v} unexpectedly vary over time:")
+                  ok = False
+                  if verbose:
+                     for thisPatch in np.where(np.bitwise_not(ok_p))[0]:
+                        thisLon = this_ds.patches1d_lon.values[thisPatch]
+                        thisLat = this_ds.patches1d_lat.values[thisPatch]
+                        thisCrop = this_ds.patches1d_itype_veg_str.values[thisPatch]
+                        thisCrop_int = this_ds.patches1d_itype_veg.values[thisPatch]
+                        thisStr = f"   Patch {thisPatch} (lon {thisLon} lat {thisLat}) {thisCrop} ({thisCrop_int})"
+                        if v == "SDATES":
+                              print(f"{thisStr}: Sowing {t1_yr} jday {int(t1_vals[thisPatch])}, {t_yr} jday {int(t_vals[thisPatch])}")
+                        else:
+                              print(f"{thisStr}: {t1_yr} {v} {int(t1_vals[thisPatch])}, {t_yr} {v} {int(t_vals[thisPatch])}")
+                  else:
+                     print(f"{v} timestep {t} does not match timestep {t1}")
+
+         if ok:
+            print(f"✅ CLM output {v} do not vary through {this_ds.dims['gs'] - t1} growing seasons of output.")
+   
+   
+   # Convert time*mxharvests axes to growingseason axis
+   this_ds_gs = convert_axes_timemxharvests_gs(this_ds, verbose)
+   
    # Check that GDDACCUM_PERHARV <= HUI_PERHARV
    if all(v in this_ds for v in ["GDDACCUM_PERHARV", "HUI_PERHARV"]):
       check_gddaccum_le_hui(this_ds, both_nan_ok=True, throw_error=True)
