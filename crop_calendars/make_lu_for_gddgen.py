bb9799ebf315379b983f4991fc4f9bc51f39fd7b
Sam Rabin <sam.rabin@gmail.com>
2022-08-18 23:00:47 -0600

    make_lu_for_gddgen.py now strips temporary variables.

--- crop_calendars/make_lu_for_gddgen.py
+++ crop_calendars/make_lu_for_gddgen.py
@@ -7,18 +7,17 @@ infile = '/Users/Shared/CESM_work/CropEvalData_ssr/landuse.timeseries_1.9x2.5_hi
 first_fake_year = 1976
 outfile = infile.replace('.nc4', f'.gddgen{first_fake_year}-2015.nc4')
 
-# %% Process and save
+# %% Process
 
 ds_in = xr.open_dataset(infile)
+ds_tmp = ds_in.sel(time=slice(first_fake_year,2015)).copy()
 
 # Where is each crop ever active?
-ds_in['AREA_CROP'] = (ds_in.AREA * ds_in.LANDFRAC_PFT * ds_in.PCT_CROP/100).transpose("time", "lsmlat", "lsmlon")
-ds_in['AREA_CFT'] = (ds_in['AREA_CROP'] * ds_in.PCT_CFT/100).transpose("time", "cft", "lsmlat", "lsmlon")
-ds_area_max = ds_in['AREA_CFT'].max(dim="time")
-ds_in['ever_active_bycft'] = (ds_area_max > 0).transpose("cft", "lsmlat", "lsmlon")
-ds_in['ever_active'] = ds_in['ever_active_bycft'].any(dim="cft")
-
-ds_tmp = ds_in.sel(time=slice(first_fake_year,2015)).copy()
+ds_tmp['AREA_CROP'] = (ds_tmp.AREA * ds_tmp.LANDFRAC_PFT * ds_tmp.PCT_CROP/100).transpose("time", "lsmlat", "lsmlon")
+ds_tmp['AREA_CFT'] = (ds_tmp['AREA_CROP'] * ds_tmp.PCT_CFT/100).transpose("time", "cft", "lsmlat", "lsmlon")
+ds_area_max = ds_tmp['AREA_CFT'].max(dim="time")
+ds_tmp['ever_active_bycft'] = (ds_area_max > 0).transpose("cft", "lsmlat", "lsmlon")
+ds_tmp['ever_active'] = ds_tmp['ever_active_bycft'].any(dim="cft")
 
 # For cells that EVER have cropland, when do they have 0 crop area? Change those 0% values to 1%.
 new_pct_crop_ar = ds_tmp['PCT_CROP'].values
@@ -30,7 +29,7 @@ ds_tmp['AREA_CROP'] = (ds_tmp.AREA * ds_tmp.LANDFRAC_PFT * ds_tmp.PCT_CROP/100).
 if np.any((ds_tmp['AREA_CROP']==0) & ds_tmp['ever_active']):
    raise RuntimeError("Failed to fill 0% CROP with 1% where needed.")
 
-# %% For cells that EVER have each CFT, when do they have 0 area of that CFT? Change those 0% values to something positive.
+# For cells that EVER have each CFT, when do they have 0 area of that CFT? Change those 0% values to something positive.
 new_pct_cft_tcyx = ds_tmp['PCT_CFT'].values
 new_pct_cft_tcyx[np.where((ds_tmp['AREA_CFT']==0) & ds_tmp['ever_active'])] = 1.0
 # Ensure sum to 100
@@ -45,7 +44,8 @@ ds_tmp['PCT_CFT'] = xr.DataArray(data = new_pct_cft_tcyx,
                                  attrs = ds_tmp['PCT_CFT'].attrs)
 
 
-# Save output netCDF
-ds_out = xr.concat((ds_in.sel(time=slice(0, first_fake_year-1)),
+# %% Save output netCDF
+ds_out = xr.concat((ds_in.copy().sel(time=slice(0, first_fake_year-1)),
                     ds_tmp), dim="time")
+ds_out = ds_out.drop([v for v in ds_out if v not in ds_in])
 ds_out.to_netcdf(outfile)
