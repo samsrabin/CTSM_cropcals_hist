b45be1f66bc9b66ad48087ac4a2d27ab15ba8867
Sam Rabin <sam.rabin@gmail.com>
2022-08-19 13:46:54 -0600

    Only call make_figures() if save_figs True.

--- crop_calendars/generate_gdds.py
+++ crop_calendars/generate_gdds.py
@@ -830,6 +830,6 @@ def make_figures(thisDir=None, gdd_maps_ds=None, gddharv_maps_ds=None):
 
     print("Done.")
 
-make_figures(gdd_maps_ds=gdd_maps_ds, gddharv_maps_ds=gdd_maps_ds)
+if save_figs: make_figures(gdd_maps_ds=gdd_maps_ds, gddharv_maps_ds=gdd_maps_ds)
 
 
