45adb38059eb29ac02d2c9af5fe1cc43ca57304d
Sam Rabin <sam.rabin@gmail.com>
2023-04-04 11:30:00 -0600

    make_surface_for_gddgen: Doing what I did before with flanduse_timeseries, except now with fsurdat.
    The new method generated "does not add to 1" errors for wt_cft in CLM. Not sure why.

--- crop_calendars/make_surface_for_gddgen.py
+++ crop_calendars/make_surface_for_gddgen.py
@@ -4,6 +4,55 @@ import argparse
 import sys
 import os
 
+# Doing what I did before with flanduse_timeseries, except now with
+# fsurdat.
+def get_new_fsurdat_v0(surf, lu, params):
+    
+    # Where is each crop ever active?
+    if "AREA" not in lu:
+        lu["AREA"] = xr.DataArray(data=np.ones_like(lu["LANDFRAC_PFT"]),
+                                  dims=lu["LANDFRAC_PFT"].dims,
+                                  coords=lu["LANDFRAC_PFT"].coords)
+    lu['AREA_CROP'] = (lu.AREA * lu.LANDFRAC_PFT * lu.PCT_CROP/100).transpose("time", "lsmlat", "lsmlon")
+    lu['AREA_CFT'] = (lu['AREA_CROP'] * lu.PCT_CFT/100).transpose("time", "cft", "lsmlat", "lsmlon")
+    ds_area_max = lu['AREA_CFT'].max(dim="time")
+    lu['ever_active_bycft'] = (ds_area_max > 0).transpose("cft", "lsmlat", "lsmlon")
+    lu['ever_active'] = lu['ever_active_bycft'].any(dim="cft")
+    
+    # For cells that EVER have cropland, when do they have 0 crop area? Change those 0% values to 1%.
+    new_pct_crop_ar = lu['PCT_CROP'].values
+    new_pct_crop_ar[np.where((lu['AREA_CROP']==0) & lu['ever_active'])] = 1.0
+    lu['PCT_CROP'] = xr.DataArray(data = new_pct_crop_ar,
+                                  coords = lu['PCT_CROP'].coords,
+                                  attrs = lu['PCT_CROP'].attrs)
+    lu['AREA_CROP'] = (lu.AREA * lu.LANDFRAC_PFT * lu.PCT_CROP/100).transpose("time", "lsmlat", "lsmlon")
+    if np.any((lu['AREA_CROP']==0) & lu['ever_active']):
+        raise RuntimeError("Failed to fill 0% CROP with 1% where needed.")
+
+    # For cells that EVER have each CFT, when do they have 0 area of that CFT? Change those 0% values to something positive.
+    new_pct_cft_tcyx = lu['PCT_CFT'].values
+    new_pct_cft_tcyx[np.where((lu['AREA_CFT']==0) & lu['ever_active'])] = 1.0
+    # Ensure sum to 100
+    i = 0
+    while np.any(~np.isclose(np.sum(new_pct_cft_tcyx, axis=1), 100.0)):
+        i+=1
+        if i > 10:
+            raise RuntimeError('too many iterations')
+        new_pct_cft_tcyx = 100 * (new_pct_cft_tcyx / np.expand_dims(np.sum(new_pct_cft_tcyx, axis=1), axis=1))
+    lu['PCT_CFT'] = xr.DataArray(data = new_pct_cft_tcyx,
+                                 coords = lu['PCT_CFT'].coords,
+                                 attrs = lu['PCT_CFT'].attrs)
+    
+    # Just take the first timestep
+    new_pct_crop_da = lu['PCT_CROP'].isel(time=0)
+    new_pct_cft_da = lu['PCT_CFT'].isel(time=0)
+    
+    # flanduse_timeseries doesn't have PCT_NATVEG
+    new_natveg_da = surf['PCT_NATVEG']
+    
+    return new_pct_crop_da, new_natveg_da, new_pct_cft_da
+
+
 # Trying to minimize the number of crop PFTs that need to be simulated.
 # CLM currently giving errors about wt_cft not summing to 1.
 def get_new_fsurdat_v1(surf, lu, params):
@@ -92,7 +141,8 @@ def main(argv):
     surf = xr.open_dataset(args.fsurdat)
     params = xr.open_dataset(args.paramfile)
 
-    new_pct_crop_da, new_natveg_da, new_pct_cft_da = get_new_fsurdat_v1(surf, lu, params)
+    # new_pct_crop_da, new_natveg_da, new_pct_cft_da = get_new_fsurdat_v1(surf, lu, params)
+    new_pct_crop_da, new_natveg_da, new_pct_cft_da = get_new_fsurdat_v0(surf, lu, params)
     
     
     #############################
